ea8163b05de7010138ecd218041bd50de8c10585
Tom Prince <123157224+TPrincy@users.noreply.github.com>
2024-02-26 09:33:47 +0000

    Code changes to camaeras

--- SOR/Assets/Script/CameraMovement.cs
+++ SOR/Assets/Script/CameraMovement.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Transform cameraPos;
+
     void Update()
     {
-        
+        transform.position = cameraPos.position;
     }
 }
