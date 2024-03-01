ba2a784dd1b3fad638edc8fa89e13064de9777d1
7aid007 <tprince3@stu.chi.ac.uk>
2024-03-01 01:39:51 +0000

    Added smooth up and down movement to object

--- SOR/Assets/Script/ObjectRotoator.cs
+++ SOR/Assets/Script/ObjectRotoator.cs
@@ -6,16 +6,28 @@ using UnityEngine;
 public class ObjectRotoator : MonoBehaviour
 {
     public float rotaSpeed;
+    public float amp;
+
+    private void Start()
+    {
+        initPos = transform.position;
+    }
 
     void Update()
     {
         rotation();
+        UDmovement();
+    }
 
-
+    private void UDmovement()
+    {
+        //Small line of code to have the collectable move up and down in the form of a sin wave making it a smooth motion
+        transform.position = new Vector3(gameObject.transform.localPosition.x, Mathf.Sin(Time.time), gameObject.transform.localPosition.z);
     }
 
     private void rotation()
     {
+        //Line of code to allow the object to slowly rotate at a constant speed determined by the rotaSpeed value
         transform.Rotate(0f, rotaSpeed * Time.deltaTime, 0f, Space.Self);
     }
 }
