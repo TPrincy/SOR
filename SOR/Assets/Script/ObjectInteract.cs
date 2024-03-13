aedba3b9c25623b10d1477fa103446cce44d1cb0
7aid007 <123157224+TPrincy@users.noreply.github.com>
2024-03-13 11:36:47 +0000

    Finsihed up the interactable object glitch

--- SOR/Assets/Script/ObjectInteract.cs
+++ SOR/Assets/Script/ObjectInteract.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectInteract : MonoBehaviour
 {
+    public bool isInRange = false;
+    public GameObject InteractText;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && isInRange)
+        {
+            print("Destroy");
+            Destroy(gameObject);
+            InteractText.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        //Ensures that when the gamestart the interact text is disabled from start
+        InteractText.SetActive(false);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        //enables the interact text when the player enters the detection box of the interactable object
+        InteractText.SetActive(true);
+    }
+
     void OnTriggerStay(Collider other)
     {
-        print("destroy");
+        isInRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isInRange = false;
+        //disables the interact text when the player leaves the detection range of the object
+        InteractText.SetActive(false);
     }
 }
